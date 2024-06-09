ea0977dd3ea0ab053eab12aa27bb5c1b15045ffa
theo <brossiertheo@live.fr>
2024-06-09 17:08:14 +0200

    Update utils.js

--- force-app/main/default/lwc/bolt/utils.js
+++ force-app/main/default/lwc/bolt/utils.js
@@ -15,11 +15,20 @@ import {
  * @param {Object} params
  * @returns {string}
  */
-export function interpolate(input,params) {
+export const _interpolate = (input, params) => {
   const names = Object.keys(params);
   const vals = Object.values(params);
   return new Function(...names, `return \`${input}\`;`)(...vals);
 }
+export const _interpolateFrom = (input, target) =>
+  interpolate(input, pick(
+    ...Array.from(input.matchAll(/\${(.*?)}/g), ([,v]) => v)
+  ).from(target))
+  
+export const interpolate = (input, params = undefined) => 
+  params === undefined
+    ? {withValuesFrom: (obj) => _interpolateFrom(input, obj)}
+    : _interpolate(input, params)
 /**
  * Will plug your styles to an html element that has the attribute [data-style] on it
  * @param {string} styles 
